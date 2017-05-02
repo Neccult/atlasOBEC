5801d365096a4d40e41a4a518ad00aa4ae6c721c
dlazarosps <douglaslazarosps@yahoo.com.br>
2017-05-02 17:19:25 -0300

    [TEST] AJAX JQuery

--- teste.php
+++ teste.php
@@ -3,6 +3,8 @@ require_once("EixoUm.php");
 
 // $teste = EixoUm::find(1, 12, 0, 0, 0, 2007);
 // $teste = EixoUm::all();
+// $teste = EixoUm::teste(1, 0, 0, 0, 2007);
+// $teste = EixoUm::getter_uf_anos(1, 0, 0, 0, 0);
 
 /*foreach (EixoUm::all() as $tupla) {
 
@@ -13,18 +15,17 @@ require_once("EixoUm.php");
 	echo '<hr />';
 }*/
 
-// $teste = EixoUm::teste(1, 0, 0, 0, 2007);
+if (!empty($_GET["uf"]))
+        $uf = $_GET["uf"];
+    else
+        $uf = 0;
 
-
-foreach (EixoUm::getter_uf_anos(1, 0, 0, 0, 0, 2014) as $tupla) {
-
-	echo '<hr />';
-		echo 'UF: '.$tupla->idUF.'<br/>';
-		echo 'Ano: '.$tupla->Ano.'<br/>';
-		echo 'Valor: '.$tupla->Valor.'<br/>';
-	echo '<hr />';
+$teste = array();
+foreach (EixoUm::getter_uf_anos(1, $uf, 0, 0, 0) as $tupla) {
+	$teste[$tupla->Ano] = $tupla->Valor;
 }
 
 // var_dump($teste);
+echo json_encode($teste);
 
 ?>
