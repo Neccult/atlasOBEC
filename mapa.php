95e1fb270f45705fab24f757e86a8e9a9040cc8a
Casanova <rodrigocasanovasv@gmail.com>
2017-07-04 21:27:09 -0300

    [UPDATE] mapa.php adicionada nova tooltip dinâmica

--- mapa.php
+++ mapa.php
@@ -1,29 +1,29 @@
 <?php 
-    
-    if (!empty($_GET["var"]))
-        $var = $_GET["var"];
-    else
-        $var = 1;
+	
+	if (!empty($_GET["var"]))
+		$var = $_GET["var"];
+	else
+		$var = 1;
 
-    if (!empty($_GET["atc"]))
-        $atc = $_GET["atc"];
-    else
-        $atc = 0;
+	if (!empty($_GET["atc"]))
+		$atc = $_GET["atc"];
+	else
+		$atc = 0;
 
-    if (!empty($_GET["cad"]))
-        $cad = $_GET["cad"];
-    else
-        $cad = 0;
+	if (!empty($_GET["cad"]))
+		$cad = $_GET["cad"];
+	else
+		$cad = 0;
 
-    if (!empty($_GET["prt"]))
-        $prt = $_GET["prt"];
-    else
-        $prt = 0;
+	if (!empty($_GET["prt"]))
+		$prt = $_GET["prt"];
+	else
+		$prt = 0;
 
-    if (!empty($_GET["ano"]))
-        $ano = $_GET["ano"];
-    else
-        $ano = 2014;
+	if (!empty($_GET["ano"]))
+		$ano = $_GET["ano"];
+	else
+		$ano = 2014;
 ?>
 
 <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
@@ -56,282 +56,256 @@
 <!--================== SVG! =================-->
 <div id="corpo" class="mapa fadeIn"></div>
 
-<!--================ TOOLTIP! ===============-->
-<div id="tooltip" class="tooltip none">
-    <p><strong class="heading"></strong></p>
-    <p><span class="size"></span></p>
-</div>
+<script type="text/javascript">
+						
+	// Mapa JS //
+	/*=== tamanho do mapa ===*/
+	var windowWidth = $(window).width(),
+		width = $('.chart').width();
+		height = width/1.4;
 
-<script>
-                        
-    // Mapa JS //
-    /*=== tamanho do mapa ===*/
-    var windowWidth = $(window).width(),
-        width = $('.chart').width();
-        height = width/1.4;
+	var shapeWidth = 30;
 
-    var shapeWidth = 30;
+	 /**** desktop! ****/
+	if(windowWidth>=1200){
+	  
+		/* cria svg */
+		var svg = d3.select("#corpo").append("svg")
+			.attr("width", width)
+			.attr("height", height);
+	   
+		/* configura projeção */
+		var projection = d3.geoMercator()
+			.center([-40, -30])             
+			.rotate([4.4, 0])               
+			.scale(550)                     
+			.translate([width/2, height/1.2]);  
 
-     /**** desktop! ****/
-    if(windowWidth>=1200){
-      
-        /* cria svg */
-        var svg = d3.select("#corpo").append("svg")
-            .attr("width", width)
-            .attr("height", height);
-       
-        /* configura projeção */
-        var projection = d3.geoMercator()
-            .center([-40, -30])             
-            .rotate([4.4, 0])               
-            .scale(550)                     
-            .translate([width/2, height/1.2]);  
+		var legendTransform = "translate(365,220)";
+		var shapeWidth = 80;
+	}
+	/**** tablet landscape! ****/
+	else if(windowWidth>=992){  
 
-        var legendTransform = "translate(365,220)";
-        var shapeWidth = 80;
-    }
-    /**** tablet landscape! ****/
-    else if(windowWidth>=992){  
+		//cria svg
+		var svg = d3.select("#corpo").append("svg")
+			.attr("width", width)
+			.attr("height", height);
+	   
+		//configura projeção
+		var projection = d3.geoMercator()
+			.center([-40, -30])             
+			.rotate([4.4, 0])               
+			.scale(400)                     
+			.translate([width / 2, height / 1.2]);
 
-        //cria svg
-        var svg = d3.select("#corpo").append("svg")
-            .attr("width", width)
-            .attr("height", height);
-       
-        //configura projeção
-        var projection = d3.geoMercator()
-            .center([-40, -30])             
-            .rotate([4.4, 0])               
-            .scale(400)                     
-            .translate([width / 2, height / 1.2]);
+		var translateX = width-150;
+		var legendTransform = "translate("+translateX+",110)";
+	}
+	/**** tablet portrait! ****/
+	else if(windowWidth>=768){  
 
-        var translateX = width-150;
-        var legendTransform = "translate("+translateX+",110)";
-    }
-    /**** tablet portrait! ****/
-    else if(windowWidth>=768){  
+		var height = width/2;
 
-        var height = width/2;
+		//cria svg
+		var svg = d3.select("#corpo").append("svg")
+			.attr("width", width)
+			.attr("height", height);
+	   
+		//configura projeção
+		var projection = d3.geoMercator()
+			.center([-40, -30])             
+			.rotate([4.4, 0])               
+			.scale(400)                     
+			.translate([width / 2, height / 1.2]);
 
-        //cria svg
-        var svg = d3.select("#corpo").append("svg")
-            .attr("width", width)
-            .attr("height", height);
-       
-        //configura projeção
-        var projection = d3.geoMercator()
-            .center([-40, -30])             
-            .rotate([4.4, 0])               
-            .scale(400)                     
-            .translate([width / 2, height / 1.2]);
+		var legendTransform = "translate(500,110)";
+	}
+	/**** mobile! ****/
+	else{  
 
-        var legendTransform = "translate(500,110)";
-    }
-    /**** mobile! ****/
-    else{  
+		var height = 200;
+		var shapeWidth = 20;
 
-        var height = 200;
-        var shapeWidth = 20;
+		//cria svg
+		var svg = d3.select("#corpo").append("svg")
+			.attr("width", width)
+			.attr("height", height);
+	   
+		//configura projeção
+		var projection = d3.geoMercator()
+			.center([-50, -28])             
+			.rotate([4.4, 0])               
+			.scale(250)                     
+			.translate([width / 1.5, height / 1.2]);
 
-        //cria svg
-        var svg = d3.select("#corpo").append("svg")
-            .attr("width", width)
-            .attr("height", height);
-       
-        //configura projeção
-        var projection = d3.geoMercator()
-            .center([-50, -28])             
-            .rotate([4.4, 0])               
-            .scale(250)                     
-            .translate([width / 1.5, height / 1.2]);
+		var legendTransform = "translate(0,10)";
+	}
 
-        var legendTransform = "translate(0,10)";
-    }
+	var path = d3.geoPath()
+		.projection(projection);
+	
+	// import colors.json file
+	var colorJSON;
+	d3.json('colors.json', function(error, data) {
+	  if(error) throw error;
 
-    var path = d3.geoPath()
-        .projection(projection);
-    
-    // import colors.json file
-    var colorJSON;
-    d3.json('colors.json', function(error, data) {
-      if(error) throw error;
+	  colorJSON = data;
+	});
 
-      colorJSON = data;
-    });
+	//variaveis configuracao query
+	var vrv = <?php echo $var; ?>;
+	var atc = <?php echo $atc; ?>;
+	var cad = <?php echo $cad; ?>;
+	var prt = <?php echo $prt; ?>;
+	var ano = <?php echo $ano; ?>;
 
-    //variaveis configuracao query
-    var vrv = <?php echo $var; ?>;
-    var atc = <?php echo $atc; ?>;
-    var cad = <?php echo $cad; ?>;
-    var prt = <?php echo $prt; ?>;
-    var ano = <?php echo $ano; ?>;
+	var config = "?var="+vrv+"&atc="+atc+"&cad="+cad+"&prt="+prt+"&ano="+ano+"";
+	// console.log(config);
 
-    var config = "?var="+vrv+"&atc="+atc+"&cad="+cad+"&prt="+prt+"&ano="+ano+"";
-    // console.log(config);
+	//pre-load arquivos
+	d3.queue()
+		.defer(d3.json, "br-min.json")
+		.defer(d3.json, "ajax_mapa.php"+config)
+		.await(ready);
 
-    //pre-load arquivos
-    d3.queue()
-        .defer(d3.json, "br-min.json")
-        .defer(d3.json, "ajax_mapa.php"+config)
-        .await(ready);
+	//leitura
+	function ready(error, br_states, mapa){
 
-    //leitura
-    function ready(error, br_states, mapa){
+		if (error) return console.error(error);
 
-        if (error) return console.error(error);
+		//variaveis informacao
+		var dict = {};
+		var info = [];
 
-        //variaveis informacao
-        var dict = {};
-        var info = [];
+		Object.keys(mapa).forEach(function(key) {
 
-        Object.keys(mapa).forEach(function(key) {
+			// console.log(key, mapa[key]);
+			info.push(mapa[key]);
+			return dict[mapa[key].id] = {id:mapa[key].id, uf:mapa[key].uf, valor:mapa[key].valor, percentual:mapa[key].percentual*100, taxa:mapa[key].taxa};
 
-            // console.log(key, mapa[key]);
-            info.push(mapa[key]);
-            return dict[mapa[key].id] = {id:mapa[key].id, uf:mapa[key].uf, valor:mapa[key].valor};
+		});
 
-        });
-
-        //carrega estados JSON
-        var states = topojson.feature(br_states, br_states.objects.states);
+		//carrega estados JSON
+		var states = topojson.feature(br_states, br_states.objects.states);
 
 
-        //exclui linha de cabeçario do OBJ
-        info.splice(0,1);
-        info.splice(27,28);
-        delete dict[0];
-        // console.log(dict);
-        // console.log(info);
+		//exclui linha de cabeçario do OBJ
+		info.splice(0,1);
+		info.splice(27,28);
+		delete dict[0];
 
-        //valores maximos e minimos
-        var minValue = d3.min(info, function(d) {return d.valor; });
-        var maxValue = d3.max(info, function(d) {return d.valor; });
+		//valores maximos e minimos
+		var minValue = d3.min(info, function(d) {return d.valor; });
+		var maxValue = d3.max(info, function(d) {return d.valor; });
 
-        //distribuicao de frequencias    
-        var quant = 9;
-        var range = maxValue - minValue; 
-        var amp = minValue < 1 && minValue > -1 ? range / quant : Math.round(range / quant);
+		//distribuicao de frequencias    
+		var quant = 9;
+		var range = maxValue - minValue; 
+		var amp = minValue < 1 && minValue > -1 ? range / quant : Math.round(range / quant);
 
-        //domino de valores para as cores do mapa
-        var dom = [
-                    (minValue+(amp/4)), 
-                    (minValue+amp), 
-                    (minValue+(2*amp)), 
-                    (minValue+(3*amp)), 
-                    (minValue+(4*amp)), 
-                    (minValue+(5*amp)), 
-                    (minValue+(6*amp)), 
-                    (minValue+(7*amp)), 
-                    (minValue+(8*amp))
-                ];
+		//domino de valores para as cores do mapa
+		var dom = [
+					(minValue+(amp/4)), 
+					(minValue+amp), 
+					(minValue+(2*amp)), 
+					(minValue+(3*amp)), 
+					(minValue+(4*amp)), 
+					(minValue+(5*amp)), 
+					(minValue+(6*amp)), 
+					(minValue+(7*amp)), 
+					(minValue+(8*amp))
+				];
 
-        //ajuste do dominio
-        var i = 0;
-        if(amp > 1){
-            while(i<=9){
-                dom[i] = dom[i] - (dom[i] % 5);
-                i++;
-            }
-        } 
-                
-        // creates cadeia's color range array from color.json file
-        var colorsRange = [];             
-        $.each(colorJSON.cadeias[cad].gradient, function(i, rgb){
-            if(i > 1)
-                colorsRange.push(rgb);
-        });
+		//ajuste do dominio
+		var i = 0;
+		if(amp > 1){
+			while(i<=9){
+				dom[i] = dom[i] - (dom[i] % 5);
+				i++;
+			}
+		} 
+				
+		// creates cadeia's color range array from color.json file
+		var colorsRange = [];             
+		$.each(colorJSON.cadeias[cad].gradient, function(i, rgb){
+			if(i > 1)
+				colorsRange.push(rgb);
+		});
 
-        var mouseOn = function(d) {
+		/*
+		{
+			1: {
+				el: '.heading',
+				data: d['properties']['name']
+			},
+			2: {
+				el: '.size',
+				data: d['properties']['dict[d.id].valor']
+			}
+		}
+		 */
+		
+		var toolFun = tooltip();
+		var tooltipEl = toolFun.addTooltip();
 
-            /* atualiza nome tooltip */
-            d3.select(".tooltip .heading")
-                .text(d['properties']['name']);
+		//coloração do mapa
+		var color = d3.scaleThreshold()
+			.domain(dom)
+			.range(colorsRange);
 
-            d3.select(".tooltip .size")
-                .text(formatNumber(dict[d.id].valor));
+		//concatena propriedades
+		svg.append("g")
+			.attr("class", "states")
+			.selectAll("path")
+			.data(states.features)
+			.enter()
+			.append("path")
+			// .style('fill', function(d){return color(d.properties.name.replace(/\s+/g, '').length);})
+			.style('fill', function(d){return color(dict[d.id].valor);})
+			.attr("d", path)
+				  
+			//mouseover
+			.on("mouseover", function(d){
+				toolFun.showTooltip(d, tooltipEl, [
+					["title", d['properties']['name']],
+					["Valor", formatNumber(dict[d.id].valor)],
+					["Percentual", formatDecimalLimit(dict[d.id].percentual, 2) + "%"],
+					["Taxa", formatDecimalLimit(dict[d.id].valor, 2)],
+				]);
+			})
+			.on("mouseout", toolFun.hideTooltip);
 
-            /*== posição do gráfico na tela ==*/
-            var chartOffset = $('.chart').offset(), 
-                leftOffset = chartOffset.left,
-                leftOffsetEnd = leftOffset+$('.chart').width(),
-                topOffset = chartOffset.top;
+		//legenda
+		var legend_svg = d3.select("#corpo svg");
 
-            /* tamanho do tooltip */
-            var tooltipWidth = $('.tooltip').width();
+		legend_svg.append("g")
+			.attr("class", "legendLinear")
+			.attr("transform", legendTransform);
 
-            /*== posição do tooltip ==*/
-            var xPosition = d3.event.pageX-leftOffset+30;
-            var xPositionEnd = xPosition+tooltipWidth;
-            var yPosition = d3.event.pageY -topOffset+5;
+		var legendLinear = d3.legendColor()
+			.title("Total de Empresas "+ano)
+			.labelFormat(d3.format(".0f"))
+			.shapeWidth(shapeWidth)
+			.shapePadding(5)
+			.orient('vertical')
+			.scale(color);
 
-           
-            /* se a posição final do tooltip for além do final da tela */
-            if(xPositionEnd>leftOffsetEnd){
-                xPosition = xPosition - tooltipWidth - 30; /* altera a posição */
-            }
+		legend_svg.select(".legendLinear").call(legendLinear);
 
-            d3.select(".tooltip")
-                .style("left", xPosition + "px")
-                .style("top", yPosition + "px");
+		var legendLabels = $('.legendCells').find('.cell').children('.label');
 
-            d3.select(".tooltip").classed("none", false);
+		$(legendLabels).each(function(i){
 
-        };
-
-        var mouseOut = function() {
-            d3.select(".tooltip").classed("none", true);
-        };
-
-        //coloração do mapa
-        var color = d3.scaleThreshold()
-            .domain(dom)
-            .range(colorsRange);
-
-        //concatena propriedades
-        svg.append("g")
-            .attr("class", "states")
-            .selectAll("path")
-            .data(states.features)
-            .enter()
-            .append("path")
-            // .style('fill', function(d){return color(d.properties.name.replace(/\s+/g, '').length);})
-            .style('fill', function(d){return color(dict[d.id].valor);})
-            .attr("d", path)
-                  
-            //mouseover
-            .on("mouseover", mouseOn)
-            .on("mouseout", mouseOut);
-
-        //legenda
-        var legend_svg = d3.select("#corpo svg");
-
-        legend_svg.append("g")
-            .attr("class", "legendLinear")
-            .attr("transform", legendTransform);
-
-        var legendLinear = d3.legendColor()
-            .title("Total de Empresas "+ano)
-            .labelFormat(d3.format(".0f"))
-            .shapeWidth(shapeWidth)
-            .shapePadding(5)
-            .orient('vertical')
-            .scale(color);
-
-        legend_svg.select(".legendLinear").call(legendLinear);
-
-        var legendLabels = $('.legendCells').find('.cell').children('.label');
-
-        $(legendLabels).each(function(i){
-
-            if (i === 0 ){
-                $(this).text('Menor que ' + formatDecimalLimit(dom[i]));
-            } 
-            else if (i === legendLabels.length - 1) {
-                $(this).text("Maior que "+formatDecimalLimit(dom[i-1]));
-            } 
-            else{
-                $(this).text("Entre " + formatDecimalLimit(dom[i]) + " e " + formatDecimalLimit(dom[i-1]));
-            }
-        });
-    };
+			if (i === 0 ){
+				$(this).text('Menor que ' + formatDecimalLimit(dom[i]));
+			} 
+			else if (i === legendLabels.length - 1) {
+				$(this).text("Maior que "+formatDecimalLimit(dom[i-1]));
+			} 
+			else{
+				$(this).text("Entre " + formatDecimalLimit(dom[i]) + " e " + formatDecimalLimit(dom[i-1]));
+			}
+		});
+	};
 </script>
