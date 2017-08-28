a6fe0e14cc26d2c631b66b476094b27129a45163
dlazarosps <douglaslazarosps@yahoo.com.br>
2017-08-28 17:14:21 -0300

    [CREATE] classe Eixo 2

---
+++ db/EixoDois.php
@@ -0,0 +1,295 @@
+<?php
+
+###	Classe que manipula as variáveis do Eixo 2 ###
+
+class EixoDois {
+
+## Atributos ##
+
+	protected static $table = 'Eixo_2';
+	private static $conn;
+
+ 	//informações Eixo_2
+	protected $id;
+	public $Numero;
+	public $idUF;
+	public $idCadeia;
+	public $idPorte;
+	public $idOcupacao;
+	public $idEscolaridade;
+	public $idEtinia;
+	public $idIdade;
+
+	public $Formalidade;
+	public $Previdencia;
+	public $Sindical;
+	public $Sexo;
+
+	public $Ano;
+	public $Valor;
+	public $Percentual;
+	public $Taxa;
+
+	//informações UF
+	public $UFNome;
+	public $UFRegiao;
+	public $UFSigla;
+
+	//informações Cadeia
+	public $CadeiaNome;
+	
+	//informações Porte
+	public $PorteNome;
+
+	//informações Ocupação
+	public $OcupacaoNome;
+
+	//informações Escolaridade
+	public $EscolaridadeNome;
+
+	//informações Etinia
+	public $EtiniaNome;
+
+	//informações Idade
+	public $IdadeNome;
+
+## Metodos ##
+	
+	/*-----------------------------------------------------------------------------
+	Função: Connect
+	    função para estabelecer conexão do objeto com o banco de dados
+	Entrada: 
+	    void
+	Saída:
+	    Positivo = Retorna PDO de conexão com o banco de dados
+	    Negativo = Erro de conexão
+	-----------------------------------------------------------------------------*/
+	public static function connect(){
+		define('DB_NOME', 'Atlas');
+		define('DB_USUARIO', 'root');
+		define('DB_SENHA', 'root');
+		// define('DB_HOST', 'localhost');
+		define('DB_HOST', '143.54.231.143');
+
+		$conexao = mysqli_connect(DB_HOST, DB_USUARIO, DB_SENHA, DB_NOME);
+
+			if (mysqli_connect_errno())
+			{
+				echo "Failed to connect to MySQL: " . mysqli_connect_error();
+			}
+
+		self::$conn = $conexao;
+		mysqli_query($conexao, 'SET NAMES utf8');
+	}
+
+	/*-----------------------------------------------------------------------------
+	Função: Disconnect
+	    função para desconectar o objeto do banco de dados
+	Entrada: 
+	    void
+	Saída:
+	    valor de retorno do mysql_close()
+	-----------------------------------------------------------------------------*/
+	public static function disconnect(){
+		mysqli_close(self::$conn);
+	}
+
+	/*-----------------------------------------------------------------------------
+	Função: Find
+	    função para buscar um conjunto de tupla no banco de dados
+	Entrada: 
+	    $var = número da váriavel 
+	    $ufs = id do UF 
+	    $cad = id do SCC 
+	    $prt = id do porte
+	    $ocp = id da ocupação
+	    $esc = id da escolaridade
+	    $etn = id do etinia
+	    $idd = id da idade
+	    $form = flag formalidade
+		$prev = flag previdencia;
+		$sind = flag sindical;
+		$sexo = flag sexos;
+	    $anos = ano 
+	Saída:
+	    Um conjunto de instâncias da Classe EixoDois com seus devidos atributos
+	-----------------------------------------------------------------------------*/
+	public static function find($var, $ufs, $cad, $prt, $ocp, $esc, $etn, $idd, $form, $prev, $sind, $sexos, $anos){
+
+		self::connect();
+
+			$query = "SELECT * FROM ".self::$table." AS ex"
+					." JOIN UF AS uf ON uf.idUF = ex.idUF AND uf.idUF = ".$ufs
+					." JOIN Cadeia AS cad ON cad.idCadeia = ex.idCadeia AND cad.idCadeia = ".$cad
+					." JOIN Porte AS prt ON prt.idPorte = ex.idPorte AND prt.idPorte = ".$prt
+					." JOIN Ocupacao AS ocp ON ocp.idOcupacao = ex.idOcupacao AND ocp.idOcupacao = ".$ocp
+					." JOIN Escolaridade AS esc ON esc.idEscolaridade = ex.idEscolaridade AND esc.idEscolaridade = ".$esc
+					." JOIN Etinia AS etn ON etn.idEtinia = ex.idEtinia AND etn.idEtinia = ".$etn
+					." JOIN Idade AS idd ON idd.idIdade = ex.idIdade AND idd.idIdade = ".$idd
+					." WHERE ex.Numero = ".$var
+					." AND ex.Formalidade = ".$form 
+					." AND ex.Previdencia = ".$prev
+					." AND ex.Sindical = ".$sind
+					." AND ex.Sexo = ".$sexos 
+					." AND ex.Ano = ".$anos;
+
+			$result = mysqli_query(self::$conn, $query);
+			$obj = mysqli_fetch_object($result, 'EixoDois');
+
+		self::disconnect();
+
+		return ($obj == false) ? NULL : $obj;
+	}
+
+	/*-----------------------------------------------------------------------------
+	Função: All
+	    função para buscar todas tupla no banco de dados
+	Entrada: 
+	    void
+	Saída:
+	    Todas instancia da Classe EixoDois com seus devidos atributos 
+	-----------------------------------------------------------------------------*/
+	public static function all(){
+		self::connect();
+			// $query = "SELECT * FROM ".self::$table." ORDER BY id";
+			$query = "SELECT * FROM ".self::$table." AS ex"
+						." JOIN UF AS uf ON uf.idUF = ex.idUF"
+						." JOIN Cadeia AS cad ON cad.idCadeia = ex.idCadeia"
+						." JOIN Porte AS prt ON prt.idPorte = ex.idPorte"
+						." JOIN Ocupacao AS ocp ON ocp.idOcupacao = ex.idOcupacao"
+						." JOIN Escolaridade AS esc ON esc.idEscolaridade = ex.idEscolaridade"
+						." JOIN Etinia AS etn ON etn.idEtinia = ex.idEtinia"
+						." JOIN Idade AS idd ON idd.idIdade = ex.idIdade"
+						." ORDER BY id";
+
+			$result = mysqli_query(self::$conn, $query);
+			$allObjects = array();
+
+			while($obj = mysqli_fetch_object($result, 'EixoDois')){
+				$allObjects[] = $obj;
+			}
+
+		self::disconnect();
+		
+		return $allObjects;
+	}
+
+	/*-----------------------------------------------------------------------------
+	Função: Getter Mapa
+	    função para obter um conjunto de tuplas para o mapa
+	Entrada: 
+	    $var = número da váriavel 
+	    $ufs = id do UF 
+	    $cad = id do SCC 
+	    $prt = id do porte
+	    $ocp = id da ocupação
+	    $esc = id da escolaridade
+	    $etn = id do etinia
+	    $idd = id da idade
+	    $form = flag formalidade
+		$prev = flag previdencia;
+		$sind = flag sindical;
+		$sexo = flag sexos;
+	    $anos = ano
+	Saída:
+	    Um conjunto de instâncias da Classe EixoDois com seus devidos atributos
+	-----------------------------------------------------------------------------*/
+	public static function getter_mapa($var, $atc, $cad, $prt, $anos){
+
+		self::connect();		
+			$query = "SELECT * FROM ".self::$table." AS ex"
+					." JOIN UF AS uf ON uf.idUF = ex.idUF"
+					." JOIN Atuacao AS atc ON atc.idAtuacao = ex.idAtuacao AND atc.idAtuacao = ".$atc
+					." JOIN Cadeia AS cad ON cad.idCadeia = ex.idCadeia AND cad.idCadeia = ".$cad
+					." JOIN Porte AS prt ON prt.idPorte = ex.idPorte AND prt.idPorte = ".$prt
+					." WHERE ex.Numero = ".$var;
+
+				$query .= ($anos > 0) ? " AND ex.Ano = ".$anos : "" ;
+
+			$result = mysqli_query(self::$conn, $query);
+			$allObjects = array();
+
+			while($obj = mysqli_fetch_object($result, 'EixoDois')){
+				$allObjects[] = $obj;
+			}
+
+		self::disconnect();
+		
+		return $allObjects;
+	}
+
+	/*-----------------------------------------------------------------------------
+	Função: Getter Barras
+	    função para obter um conjunto de tuplas para o barras
+	Entrada: 
+	    $var = número da váriavel 
+	    $ufs = id do UF 
+	    $atc = id da atuação
+	    $cad = id do SCC 
+	    $prt = id do porte
+	Saída:
+	    Um conjunto de instâncias da Classe EixoDois com seus devidos atributos
+	-----------------------------------------------------------------------------*/
+	public static function getter_barras($var, $ufs, $atc, $cad, $prt){
+
+		self::connect();		
+			$query = "SELECT * FROM ".self::$table." AS ex"
+					." JOIN UF AS uf ON uf.idUF = ex.idUF AND uf.idUF = ".$ufs
+					." JOIN Atuacao AS atc ON atc.idAtuacao = ex.idAtuacao AND atc.idAtuacao = ".$atc
+					." JOIN Cadeia AS cad ON cad.idCadeia = ex.idCadeia AND cad.idCadeia = ".$cad
+					." JOIN Porte AS prt ON prt.idPorte = ex.idPorte AND prt.idPorte = ".$prt
+					." WHERE ex.Numero = ".$var;
+
+			$result = mysqli_query(self::$conn, $query);
+			$allObjects = array();
+
+			while($obj = mysqli_fetch_object($result, 'EixoDois')){
+				$allObjects[] = $obj;
+			}
+
+		self::disconnect();
+		
+		return $allObjects;
+	}
+	
+
+	/*-----------------------------------------------------------------------------
+	Função: Getter Region
+	    função para obter um conjunto de tuplas para treemap region
+	Entrada: 
+	    $var = número da váriavel 
+	    $atc = id da atuação
+	    $cad = id do SCC 
+	    $prt = id do porte
+	    $anos = ano 
+	    $regiao = região do Brasil
+	Saída:
+	    Um conjunto de instâncias da Classe EixoDois com seus devidos atributos
+	-----------------------------------------------------------------------------*/
+	public static function getter_region($var, $atc, $cad, $prt, $anos, $regiao){
+
+		self::connect();		
+			$query = "SELECT * FROM ".self::$table." AS ex"
+					." JOIN UF AS uf ON uf.idUF = ex.idUF AND uf.UFRegiao LIKE '".$regiao."'"
+					." JOIN Atuacao AS atc ON atc.idAtuacao = ex.idAtuacao AND atc.idAtuacao = ".$atc
+					." JOIN Cadeia AS cad ON cad.idCadeia = ex.idCadeia AND cad.idCadeia = ".$cad
+					." JOIN Porte AS prt ON prt.idPorte = ex.idPorte AND prt.idPorte = ".$prt
+					." WHERE ex.Numero = ".$var;
+
+				$query .= ($anos > 0) ? " AND Ano = ".$anos : "" ;
+
+			$result = mysqli_query(self::$conn, $query);
+			$allObjects = array();
+
+			while($obj = mysqli_fetch_object($result, 'EixoDois')){
+				$allObjects[] = $obj;
+			}
+
+		self::disconnect();
+		
+		return $allObjects;
+	}
+
+}
+
+?>
