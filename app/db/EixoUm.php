9ad37f869055eeaaa5dfd2630f5fbf7d8a228585
Fernão Lopes <fernao@riseup.net>
2018-05-14 12:10:33 -0300

    [FIX] Implenta modo correto de puxar dados no mysqli

--- app/db/EixoUm.php
+++ app/db/EixoUm.php
@@ -104,9 +104,10 @@ class EixoUm {
 		$query = "SELECT MAX(Ano) AS Ano, Numero FROM `Eixo_1` WHERE `idUF` = 0  GROUP BY Numero";
         $stmt = mysqli_stmt_init(self::$conn);
         mysqli_stmt_prepare($stmt, $query);        
+        $stmt->execute();
         
 		self::disconnect();
-
+        
 		$allObjects = array();
         $allObjects = self::fetch_results($stmt);
 
