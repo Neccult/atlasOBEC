8ef4277e20dc1c7dee03c5181881f26d98f17979
Fernão Lopes <fernao@riseup.net>
2018-05-22 11:10:47 -0300

    [FIX] Eixo 1: correção na query

--- app/db/EixoUm.php
+++ app/db/EixoUm.php
@@ -126,7 +126,7 @@ class EixoUm {
 
         $params[] = $var;
         $params[] = $cad;
-        $params[] = $cad;
+        $params[] = $prt;
 
         $paramsStr = '';
         foreach ($params as $param) {
