f60bca2f12d8f9d3aa1d382e46cc91dfc54d48cc
leodorna <leonardo.dorneles@inf.ufrgs.br>
2018-06-05 09:30:22 -0300

    [UPDATE] Get URL

--- app/js/load_views.js
+++ app/js/load_views.js
@@ -1,52 +1,22 @@
+parameters = {}
 
-var parameters = {
-    'eixo':$('.bread-select[data-id=eixo]').prop('selectedIndex'),
-    'var': $('.bread-select[data-id=var]').val(),
-    'uf' : $('.bread-select[data-id=uf').val(),
-    'ano': $('.bread-select[data-id=ano]').val(),
-    'cad': $('.bread-select[data-id=cad]').val(),
-    'prt': 0,
-    'deg': 0,
-    'sub_deg': 0,
-    'ocp': 0
-}
+window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value){
+    parameters[key] = value;
+})
 
-GLOBAL_DATA = {
-    'box1': [],
-    'box2': [],
-    'box3': []
-}
+parameters.eixo = parameters.eixo.replace(/#.*/, '')
 
 PT_BR = []
 COLORS = []
 URL_PARAM = $.param(parameters);
 
-switch(parameters.eixo){
-    case 0:
-        parameters.prt = $('.bread-select[data-id=deg]').val() 
-        break;
-    case 1: 
-        if($(".bread-select[data-id=deg]").find('option:selected').parent().attr("value") != undefined){
-            parameters.deg = $(".bread-select[data-id=deg]").find('option:selected').parent().attr("value")
-            parameters.sub_deg = $(".bread-select[data-id=deg]").val()
-        } else {
-            parameters.deg = $(".bread-select[data-id=deg]").val()
-        }
-        parameters.ocp = $('.bread-select[data-id=ocp]').val()
-        break;
-    case 2: 
-        break;
-    case 3: 
-        break;
-}
-
-
 $.when($.get('data/pt-br.json'), $.get('data/colors.json')).done(function(pt_br_JSON, colors_JSON){
     PT_BR = pt_br_JSON[0]
     COLORS = colors_JSON[0]
-    
-    $.getScript('js/teste_global_var.js')
-    $.getScript('js/teste_global_var.js')
-    $.getScript('js/teste_global_var.js')
+    data_var = getDataVar(PT_BR, parameters.eixo, parameters.var)
+
+    $.getScript('js/'+data_var.views.view_box1[0]+'.js')
+    $.getScript('js/'+data_var.views.view_box2[0]+'.js')
+    $.getScript('js/'+data_var.views.view_box3[0]+'.js')
     
 })
